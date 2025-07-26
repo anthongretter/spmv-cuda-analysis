de8b33484ef1adb1380fa30cbb83261e574247e7
anthongretter <anthon.png@proton.me>
2025-07-26 18:45:08 +0200

    unroll only last 16

--- src/imp/gpu_unrl.cu
+++ src/imp/gpu_unrl.cu
@@ -4,13 +4,7 @@
 template<unsigned int unroll_factor>
 __device__ __forceinline__ PRIM_T compute_unrolled_sum(volatile struct CSR *csr, VEC_T vec, int &j) {
     PRIM_T sum = 0;
-    
-    if constexpr (unroll_factor >= 22) sum += csr->val[j + 21] * vec[csr->col[j + 21]];
-    if constexpr (unroll_factor >= 21) sum += csr->val[j + 20] * vec[csr->col[j + 20]];
-    if constexpr (unroll_factor >= 20) sum += csr->val[j + 19] * vec[csr->col[j + 19]];
-    if constexpr (unroll_factor >= 19) sum += csr->val[j + 18] * vec[csr->col[j + 18]];
-    if constexpr (unroll_factor >= 18) sum += csr->val[j + 17] * vec[csr->col[j + 17]];
-    if constexpr (unroll_factor >= 17) sum += csr->val[j + 16] * vec[csr->col[j + 16]];
+
     if constexpr (unroll_factor >= 16) sum += csr->val[j + 15] * vec[csr->col[j + 15]];
     if constexpr (unroll_factor >= 15) sum += csr->val[j + 14] * vec[csr->col[j + 14]];
     if constexpr (unroll_factor >= 14) sum += csr->val[j + 13] * vec[csr->col[j + 13]];
@@ -43,103 +37,12 @@ __global__ void SPMV_kernel(int row, int col, int n, void* ptr_matrix, VEC_T vec
         int row_end = csr->row[thread_row + 1];
         int j = row_start;
 
-        while (j < row_end) {
-            int remaining = row_end - j;
-
-            // Pick the largest unroll factor that fits
-            switch (remaining) {
-                default: // >=22
-                    sum += compute_unrolled_sum<22>(csr, vec, j);
-                    j += 22;
-                    break;
-                case 21:
-                    sum += compute_unrolled_sum<21>(csr, vec, j);
-                    j += 21;
-                    break;
-                case 20:
-                    sum += compute_unrolled_sum<20>(csr, vec, j);
-                    j += 20;
-                    break;
-                case 19:
-                    sum += compute_unrolled_sum<19>(csr, vec, j);
-                    j += 19;
-                    break;
-                case 18:
-                    sum += compute_unrolled_sum<18>(csr, vec, j);
-                    j += 18;
-                    break;
-                case 17:
-                    sum += compute_unrolled_sum<17>(csr, vec, j);
-                    j += 17;
-                    break;
-                case 16:
-                    sum += compute_unrolled_sum<16>(csr, vec, j);
-                    j += 16;
-                    break;
-                case 15:
-                    sum += compute_unrolled_sum<15>(csr, vec, j);
-                    j += 15;
-                    break;
-                case 14:
-                    sum += compute_unrolled_sum<14>(csr, vec, j);
-                    j += 14;
-                    break;
-                case 13:
-                    sum += compute_unrolled_sum<13>(csr, vec, j);
-                    j += 13;
-                    break;
-                case 12:
-                    sum += compute_unrolled_sum<12>(csr, vec, j);
-                    j += 12;
-                    break;
-                case 11:
-                    sum += compute_unrolled_sum<11>(csr, vec, j);
-                    j += 11;
-                    break;
-                case 10:
-                    sum += compute_unrolled_sum<10>(csr, vec, j);
-                    j += 10;
-                    break;
-                case 9:
-                    sum += compute_unrolled_sum<9>(csr, vec, j);
-                    j += 9;
-                    break;
-                case 8:
-                    sum += compute_unrolled_sum<8>(csr, vec, j);
-                    j += 8;
-                    break;
-                case 7:
-                    sum += compute_unrolled_sum<7>(csr, vec, j);
-                    j += 7;
-                    break;
-                case 6:
-                    sum += compute_unrolled_sum<6>(csr, vec, j);
-                    j += 6;
-                    break;
-                case 5:
-                    sum += compute_unrolled_sum<5>(csr, vec, j);
-                    j += 5;
-                    break;
-                case 4:
-                    sum += compute_unrolled_sum<4>(csr, vec, j);
-                    j += 4;
-                    break;
-                case 3:
-                    sum += compute_unrolled_sum<3>(csr, vec, j);
-                    j += 3;
-                    break;
-                case 2:
-                    sum += compute_unrolled_sum<2>(csr, vec, j);
-                    j += 2;
-                    break;
-                case 1:
-                    sum += compute_unrolled_sum<1>(csr, vec, j);
-                    j += 1;
-                    break;
-                case 0:
-                    break;
-            }
+        while (j - row_end > 16) {
+            sum += csr->val[j] * vec[csr->col[j]];
+            j++;
         }
+        
+        sum += compute_unrolled_sum<16>(csr, vec, row_end, j);
 
         // Handle remaining elements
         // for (; j < row_end; j++) {
